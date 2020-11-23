1c8dea1ba33c999d30642505d81f8ccd7d2c64be
koenvanderels <koen.vanderels@eonics.nl>
2020-11-23 18:38:26 +0100

    added db link

--- WebApplication2/Controllers/StudentController.cs
+++ WebApplication2/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication2.Data;
 using WebApplication2.Models;
@@ -8,14 +9,6 @@ namespace WebApplication2
     public class StudentController : Controller
     {
         private MijnContext context;
-        private static List<Student> students = new List<Student>()
-        {
-            new Student(19026048, "Koen", "koen@camptoo.nl"),
-            new Student(19000502, "Robin", "robin@heidenis.com"),
-            new Student(18048730, "Binh", "binh@gmail.com"),
-            new Student(19082517, "Mehdi", "binh@gmail.com"),
-            new Student(19026048, "Koen", "koen@camptoo.nl")
-        };
 
         public StudentController(MijnContext context) 
         {
@@ -30,67 +23,43 @@ namespace WebApplication2
         public IActionResult Aantal(string voornaam)
         {
             ViewData["voornaam"] = voornaam;
-            ViewData["aantal"] = CountNames(voornaam);
-            // OR
-            // ViewData["aantal"] = students.FindAll(s => s.firstName.ToLower() == voornaam.ToLower()).Count;
+            ViewData["aantal"] = context.students.Count(s => s.firstName.ToLower() == voornaam.ToLower());
             return View();
-        }
-
-        private int CountNames(string voornaam)
-        {
-            int countNames = 0;
-            foreach (Student student in students)
-            {
-                if (student.firstName.ToLower() == voornaam.ToLower()) countNames++;
-            }
-
-            return countNames;
         }
 
         public IActionResult Email(int id)
         {
-            ViewData["email"] = findEmailByID(id);
-            // OR
-            // ViewData["email"] = students.Find(s => s.studentNumber == id)?.email;
-
+            ViewData["email"] = context.students.Single(s => s.studentNumber == id).email;
             return View();
         }
 
-        private string findEmailByID(int id)
-        {
-            foreach (Student student in students)
-            {
-                if (id == student.studentNumber)
-                {
-                    return student.email;
-                }
-            }
-            return "";
-        }
+
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(int nummer, string voornaam, string email)
+        public IActionResult Create(Student student)
         {
-            AddStudentToList(new Student(nummer, voornaam, email));
-            return RedirectToAction("Details", new { id = nummer });
-        }
-
-        private void AddStudentToList(Student student)
-        {
-            if (student.studentNumber != 0 && student.firstName != null && student.email != null)
+            if (ModelState.IsValid)
             {
-                students.Add(student);
+                context.students.Add(student);
+                context.SaveChanges();
+                return RedirectToAction("Details", new { id = student.studentNumber });
+
+            }
+            else
+            {
+                ViewData["error"] = "invalid Input!";
+                return View();
             }
         }
-
+        
         [HttpGet]
-        public IActionResult Update(int id) 
+        public IActionResult Update(int id)
         {
-            Student student = findStudentById(id);
+            Student student = context.students.Single(s => s.studentNumber == id);
 
             return View(student);
         }
@@ -98,24 +67,17 @@ namespace WebApplication2
         [HttpPost]
         public IActionResult Update(Student inputStudent) 
         {
-            Student student = findStudentById(inputStudent.studentNumber);
+            Student student = context.students.Single(s => s.studentNumber == inputStudent.studentNumber);
 
             student.firstName = inputStudent.firstName;
             student.email = inputStudent.email;
 
             return RedirectToAction("Details", new { id = student.studentNumber });
         }
-
-        private Student findStudentById(int id) 
-        {
-            foreach (Student student in students) if (id == student.studentNumber) return student;
-            return null;
-        }
-
+        
         public IActionResult Details(int id) 
         {
-            Student student = findStudentById(id);
-
+            Student student = context.students.Single(s => s.studentNumber == id);
             return View(student);
         }
     }
