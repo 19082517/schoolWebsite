70ad91f8dd95d2a43af06958da4ed2668181f9a4
Mehdi <19082517@student.hhs.nl>
2020-11-23 19:12:24 +0100

    fixed student administrator controller

--- WebApplication2/Controllers/StudentAdministratieController.cs
+++ WebApplication2/Controllers/StudentAdministratieController.cs
@@ -8,31 +8,15 @@ namespace WebApplication2
     public class StudentAdministratieController : Controller
     {
         private MijnContext context;
-        private List<Student> students = new List<Student>();
-        
+
         public StudentAdministratieController(MijnContext context)
         {
             this.context = context;
-
-            students.Add(new Student(19026048, "Koen", "koen@camptoo.nl"));
-            students.Add(new Student(19000502, "Robin", "robin@heidenis.com"));
-            students.Add(new Student(18048730, "Binh", "binh@gmail.com"));
-            students.Add(new Student(19082517, "Mehdi", "binh@gmail.com"));
-            students.Add(new Student(19026048, "Koen", "koen@camptoo.nl"));
         }
-        
+
         public IActionResult ZoekStudenten(char id)
         {
-            List<Student> sList = new List<Student>();
-            foreach (Student s in students)
-            {
-                if (s.firstName.ToLower().StartsWith(id))
-                {
-                    sList.Add(s);
-                }
-            }
-            // OR
-            // List<Student> sList = students.FindAll(s => s.firstName.ToLower().StartsWith(id));
+            List<Student> sList = context.students.Where(s => s.firstName.ToLower().StartsWith(id)).ToList();
             return View(sList);
         }
     }
